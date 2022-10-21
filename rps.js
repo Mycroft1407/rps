4332159e0d5a1ca648dfd70e7ec0934da72f0612
Mycroft1407 <jakuszalex@gmail.com>
2022-10-21 14:30:17 +0100

    created if else function

--- rps.js
+++ rps.js
@@ -11,3 +11,33 @@ function getComputerChoice () {
     let choice = rps[Math.floor(Math.random()*rps.length)];
     console.log(choice);
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection.toLowerCase;
+    if (playerSelection == "rock" && computerSelection == "Scissors") {
+        return "You win! Rock beats scissors";
+    } else if (playerSelection == "paper" && computerSelection == "Rock") {
+        return "You win! Paper beats rock"; 
+    } else if (playerSelection == "scissors" && computerSelection == "Paper") {
+        return "You win! Scissors beats paper";
+    } else if (playerSelection == "rock" && computerSelection == "Paper") {
+        return "You lose! Paper beats rock"; 
+    } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+        return "You lose! Scissors beats paper";
+    } else if (playerSelection == "scissors" && computerSelection == "Rock") {
+        return "You lose! Rock beats scissors";
+    } else if (playerSelection == "rock" && computerSelection == "Rock" || playerSelection == "paper" && computerSelection == "Paper" || playerSelection == "scissors" && computerSelection == "Scissors") {
+        return "Draw!"
+    } else {
+        console.log("Game over!")
+    } }
+  
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+
+}
